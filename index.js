c2c847756e7c99aee969519bbb1a6212e482a732
Laurel Herting <laurel.herting@gmail.com>
2017-07-07 18:02:21 -0600

    initial commit

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello world')
 })
 
-app.get('/yo', function (req, res) {
-  res.send('YO!')
+app.get('/ye', function (req, res) {
+  res.send('YE!')
 })
 
 let server = app.listen(3000, function () {
