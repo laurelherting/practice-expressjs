521804cf830f619f10cd94c41a7f1f26a3396f29
Laurel Herting <laurel.herting@gmail.com>
2017-07-13 11:41:59 -0600

    basic routing, convert from es5 to es6

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 let express = require('express')
 let app = express()
 
+let fs = require('fs')
+let _ = require('lodash')
+let users = []
+
+fs.readFile('users.json', {encoding: 'utf8'}, function (err, data) {
+  if (err) throw err
+
+  JSON.parse(data).forEach(function (user) {
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+    users.push(user)
+  })
+
+})
+
+app.get('/', function (req, res) {
+  let buffer = ''
+
+  users.forEach(function (user) {
+    buffer += '<a href="/' + user.username + '">' + user.name.full + '</a><br>'
+  })
+  res.send(buffer)
+})
+
 app.get('/', function (req, res) {
   res.send('Hello world')
 })
 
 app.get('/ye', function (req, res) {
   res.send('YE!')
+})
+
+app.get('/:username', function (req, res) {
+  let username = re.params.username
+  res.send(username)
 })
 
 let server = app.listen(3000, function () {
