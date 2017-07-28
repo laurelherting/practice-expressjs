9793eef37217a49da338c6373ee66936215c092b
Laurel Herting <laurel.herting@gmail.com>
2017-07-28 14:20:25 -0600

    condense individual subpaths using .router

--- index.js
+++ index.js
@@ -8,36 +8,6 @@ const engines = require('consolidate');
 const helpers = require('./helpers');
 const bodyParser = require('body-parser');
 
-function getUser(username) {
-  const user = JSON.parse(fs.readFileSync(getUserFilePath(username, { encoding: 'utf8' })));
-  user.name.full = _.startCase(user.name.first + ' ' + user.name.last);
-  _.keys(user.location).forEach((key) => {
-    user.location[key] = _.startCase(user.location[key]);
-  });
-  return user;
-};
-
-function getUserFilePath(username) {
-  return path.join(__dirname, 'users', username) + '.json';
-};
-
-function saveUser(username, data) {
-  const fp = getUserFilePath(username);
-  fs.unlinkSync(fp) // delete the file
-  fs.writeFileSync(fp, JSON.stringify(data, null, 2), {encoding: 'utf8'})
-};
-
-function verifyUser (req, res, next) {
-  const fp = getUserFilePath(req.params.username);
-  
-  fs.exists(fp, (yes) => {
-    if(yes) {
-      next()
-    } else {
-      res.redirect('/error/' + req.params.username)};
-  });
-};
-
 app.engine('hbs',engines.handlebars);
 
 // EH: my personal preference
@@ -56,11 +26,11 @@ app.get('/', (req, res) => {
   const users = [];
   fs.readdir('users', (err, files) => {
     console.log('files', files);
-    if (err) throw err;
+    if (err) throw err
     files.forEach((file) => {
       console.log('file', file);
       fs.readFile(path.join(__dirname, 'users', file), { encoding: 'utf8' }, (err, data) => {
-        if (err) throw err;
+        if (err) throw err
         const user = JSON.parse(data);
         // console.log('user', user.username);
         user.name.full = _.startCase(user.name.first + ' ' + user.name.last);
@@ -86,35 +56,10 @@ app.get('/error/:username', (req, res) => {
   res.status(404).send('No user named ' + req.params.username + ' found')
 });
 
-app.route('/:username')
-app.all('/:username', (req, res, next) => {
-  console.log(req.method, 'for', req.params.username);
-  next()
-});
+const userRouter = require('./username');
+app.use('/:username', userRouter);
 
-app.get(helpers.verifyUser, (req, res) => {
-  const username = req.params.username;
-  const user = helpers.getUser(username);
-  res.render('user', {
-    user: user,
-    address: user.location
-  });
-});
 
-app.put('/:username', (req, res) => {
-  const username = req.params.username;
-  const user = getUser(username);
-  user.location = req.body;
-  helpers.saveUser(username, user);
-  res.end(); 
-});
-
-app.delete('/:username', (req,res) => {
-  const fp = helpers.getUserFilePath(req.params.username);
-  fs.unlinkSync(fp) // delete the file
-  res.sendStatus(200)
-});
-
-let server = app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log('Server running at http://localhost:' + server.address().port)
 });
